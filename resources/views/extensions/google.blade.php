78ebe90c1f077b991bc3100c1aefd5d37b3d353b
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2020-12-07 20:12:35 +0400

    add google analytics

--- resources/views/extensions/google.blade.php
+++ resources/views/extensions/google.blade.php
@@ -1,9 +1,9 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-HXQVW3VM84"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-NXM49N9457"></script>
 <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
 
-    gtag('config', 'G-HXQVW3VM84');
+    gtag('config', 'G-NXM49N9457');
 </script>
