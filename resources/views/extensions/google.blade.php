7ed7404e1a5846d47401dd92d7c9ff4e1f24ca8d
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2020-12-23 19:12:37 +0400

    google tag manager

--- resources/views/extensions/google.blade.php
+++ resources/views/extensions/google.blade.php
@@ -1,6 +1,7 @@
 @if(app()->environment() === 'production')
     <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-NXM49N9457"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-184912914-1">
+    </script>
     <script>
         window.dataLayer = window.dataLayer || [];
 
@@ -10,6 +11,6 @@
 
         gtag('js', new Date());
 
-        gtag('config', 'G-NXM49N9457');
+        gtag('config', 'UA-184912914-1');
     </script>
 @endif
