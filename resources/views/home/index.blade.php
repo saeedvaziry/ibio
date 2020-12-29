1f371ce508e9d5eb506acc3952461935811d0a77
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2020-12-29 16:48:06 +0400

    hmoe ui fix

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -20,20 +20,17 @@
                     حتی میتونی توی اینستاگرام بیشتر از یک لینک داشته باشی 😎
                 </p>
                 <form action="{{ route('register') }}" class="flex flex-col md:flex-row justify-center lg:justify-end mb-10 ltr">
-                    <div class="relative flex items-center mb-3 md:mb-0">
+                    <div class="relative flex items-center mb-3 md:mb-0 w-full md:w-60">
                         <span class="text-xl absolute ml-4 text-gray-400 ">ibio.link/</span>
                         <input type="text" required name="username" placeholder="yourname" class="bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-black focus:bg-white placeholder-gray-800 rounded-lg py-4 pr-2 block w-full text-xl" style="padding-left: 5.9rem" autocomplete="off">
                     </div>
                     <button type="submit" class="bg-purple-600 hover:bg-purple-400 transition-colors focus:outline-none text-md text-white py-3 px-6 rounded-lg md:ml-2" style="min-width: 130px;">ایجاد لینک</button>
                 </form>
             </div>
-            <div class="w-full lg:w-1/2 lg:mr-5 flex items-center justify-center lg:justify-end">
-                <img src="{{ asset('static/images/sample-mobile.png') }}" alt="ibio.link" class="rounded-3xl border-8 border-gray-900 shadow-2xl w-40 lg:w-52 ml-5">
-                <img src="{{ asset('static/images/instagram-frame.png') }}" alt="ibio.link" class="rounded-3xl border-8 border-gray-900 shadow-2xl w-40 lg:w-52">
+            <div class="w-full flex items-center justify-center lg:justify-end mr-5">
+                <img src="{{ asset('static/images/sample-mobile.png') }}" alt="ibio.link" class="rounded-3xl border-8 border-gray-900 shadow-2xl w-40 xl:w-52 ml-5">
+                <img src="{{ asset('static/images/instagram-frame.png') }}" alt="ibio.link" class="rounded-3xl border-8 border-gray-900 shadow-2xl w-40 xl:w-52">
             </div>
-{{--            <div class="w-full mx-auto lg:mr-5 flex items-center justify-center">--}}
-{{--                <iframe class="max-w-full h-80 shadow-xl rounded-xl" style="width: 609px" src="https://www.aparat.com/video/video/embed/videohash/yPQuk/vt/frame" allowFullScreen="true"></iframe>--}}
-{{--            </div>--}}
         </div>
     </section>
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 150"> <path fill="#f4edfa" fill-opacity="1" d="M0,96L48,106.7C96,117,192,139,288,128C384,117,480,75,576,69.3C672,64,768,96,864,96C960,96,1056,64,1152,48C1248,32,1344,32,1392,32L1440,32L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path> </svg>
@@ -69,7 +66,7 @@
     <section class="relative px-5 mx-auto w-full py-20">
         <h2 class="text-2xl mb-10 text-center">همین الان لینکتو دریافت کُن</h2>
         <form action="{{ route('register') }}" class="flex flex-col md:flex-row justify-center mb-10 ltr">
-            <div class="relative flex items-center mb-3 md:mb-0">
+            <div class="relative flex items-center mb-3 md:mb-0 w-full md:w-60">
                 <span class="text-xl absolute ml-4 text-gray-400 ">ibio.link/</span>
                 <input type="text" required name="username" placeholder="yourname" class="bg-gray-100 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-black focus:bg-white placeholder-gray-800 rounded-lg py-4 pr-2 block w-full text-xl" style="padding-left: 5.9rem" autocomplete="off">
             </div>
