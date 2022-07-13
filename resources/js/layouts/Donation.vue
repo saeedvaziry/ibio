d5b1fbbddd0b3cfa6452e0b1a9acdace2dcac953
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2022-07-13 21:02:40 +0200

    disable donation

--- resources/js/layouts/Donation.vue
+++ resources/js/layouts/Donation.vue
@@ -3,6 +3,9 @@
         <div class="flex items-center justify-between mb-5">
             <v-title>حمایت مالی</v-title>
         </div>
+        <div class="p-4 rounded-md mb-5 bg-red-100 text-black text-md">
+            متاسفانه بدلیل مشکلات دریافت درگاه، بخش حمایت مالی آی بیو در آپدیت بعدی غیرفعال و حذف خواهد شد. برای مشاهده تراکنشها میتونید به پنل کاربریتون توی pay.ir یا jeeb.io مراجعه کنید.
+        </div>
         <div class="mb-5">
             <v-button @click="$inertia.visit(route('donation.settings'), {preserveScroll: true})" :type="$page.props.subMenu === 'settings' ? 'secondary' : 'text'" class="inline-flex ml-2 mb-2" small>تنظیمات</v-button>
             <v-button @click="$inertia.visit(route('donation.settings.gateway-rial'), {preserveScroll: true})" :type="$page.props.subMenu === 'gateway-rial' ? 'secondary' : 'text'" class="inline-flex ml-2 mb-2" small>تنظیمات درگاه ریالی</v-button>
