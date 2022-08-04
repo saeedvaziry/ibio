145bc08f0ba54567e45663e92aab95e429ace222
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2022-08-04 20:39:25 +0200

    revert

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -34,9 +34,7 @@ class Handler extends ExceptionHandler
     public function register()
     {
         $this->reportable(function (Throwable $e) {
-            if (app()->bound('sentry')) {
-                app('sentry')->captureException($e);
-            }
+            //
         });
     }
 }
