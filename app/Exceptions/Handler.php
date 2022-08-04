3eff69989857e87e99aa8d4c4d434a48f04cab62
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2022-08-04 20:37:03 +0200

    revert sentry

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -34,7 +34,9 @@ class Handler extends ExceptionHandler
     public function register()
     {
         $this->reportable(function (Throwable $e) {
-            //
+            if (app()->bound('sentry')) {
+                app('sentry')->captureException($e);
+            }
         });
     }
 }
