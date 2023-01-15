d08c66e317e2d2eab1674a1abef54132705f0558
Saeed Vaziry <saeed.vaziry@metro-markets.de>
2023-01-15 21:06:32 +0100

    add sentry

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -44,7 +44,9 @@ class Handler extends ExceptionHandler
     public function register()
     {
         $this->reportable(function (Throwable $e) {
-            //
+            if (app()->bound('sentry')) {
+                app('sentry')->captureException($e);
+            }
         });
     }
 }
