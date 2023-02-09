afe2c51052db97370b73ff5f2596b517880773cc
Saeed Vaziry <saeed.vaziry@metro-markets.de>
2023-02-09 23:32:31 +0100

    don't report crawler exceptions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -22,7 +22,7 @@ class Handler extends ExceptionHandler
      * @var array<int, class-string<\Throwable>>
      */
     protected $dontReport = [
-        //
+        CrawlerDetectException::class
     ];
 
     /**
