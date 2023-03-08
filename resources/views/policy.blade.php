de41dba3797e3b01f8d757f012ef96e2f3984045
Saeed Vaziry <saeed.vaziry@metro-markets.de>
2023-03-08 19:56:39 +0100

    update policy

--- resources/views/policy.blade.php
+++ resources/views/policy.blade.php
@@ -25,6 +25,18 @@
             <p>Deleted links are not possible to be restored. When you delete a link, It will be deleted completely from our database. Also it's metrics (visitor information) will be deleted.</p>
             <h2>Account Deletion</h2>
             <p>Deleted accounts are not possible to be restored. When you delete your account, We will erase the account and everything related to that account including (links and metrics) from our database.</p>
+            <h2>Google Analytics</h2>
+            <p>
+                We use Google Analytics to collect information about how visitors use our website. Google Analytics uses cookies to collect information about your use of the website, including your IP address, the pages you visit, and other clickstream data. We use this information to improve our website and to better understand our visitors' needs.
+            </p>
+            <p>
+                Google Analytics may also collect information about the device you use to access our website, such as the type of device and operating system you are using, your browser type and version, and your device's screen size. This information is used to improve our website's performance and to ensure that our website displays correctly on your device.
+            </p>
+            <p>
+                You can learn more about how Google uses your data by visiting Google's Privacy Policy page.
+            </p>
+            <h2>Changes to This Privacy Policy</h2>
+            <p>We may update this Privacy Policy from time to time.</p>
         </div>
     </div>
 @endsection
